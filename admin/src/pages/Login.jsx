9032a32cb2489127619f4496e41834417f498e03
sumira2222 <sumirakhatiwoda23@gmail.com>
2026-08-07 09:50:12 +0545

    admin added

---
+++ admin/src/pages/Login.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+export default function Login() {
+    const[state,setState]=useState('Admin')
+  
+  
+  
+    return (
+   <form className='min-h-[80vh] flex items-center '>
+    <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96  border border-gray-100 rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
+        <p className='text-2xl font-semibold m-auto'><span className='text-primary'>  {state} </span> Login </p>
+        <div className='w-full'>
+            <p>Email</p>
+            <input 
+            className='border border-[#DADADA] rounded w-full p-2 mt-1'
+            
+            type="email" required />
+        </div>
+           <div className='w-full'>
+            <p className='w-full'>Password</p>
+            <input
+            className='border border-[#DADADA] rounded w-full p-2 mt-1'
+             type="password" required />
+        </div>
+        <button>Login</button>
+    </div>
+   </form>
+  )
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,10 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: { port: 5173 },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
 })
