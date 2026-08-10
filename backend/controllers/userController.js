db362c40c665ed515e256acc8607975ab146e6da
sumira2222 <sumirakhatiwoda23@gmail.com>
2026-08-10 23:39:39 +0545

    user updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,10 @@
-
-
-
-// API to register user
-
 import validator from "validator"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import userModel from "../models/userModel.js"
+import {v2 as cloudinary} from 'cloudinary'
+import doctorModel from "../models/doctorModel.js"
+
 
 
 // API to register user
@@ -42,6 +40,8 @@ const registerUser = async (req,res) => {
         const newUser = new userModel(userData)
         const user = await newUser.save()
 
+
+        // _id
         const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
 
         res.json({success:true, token})
@@ -52,4 +52,147 @@ const registerUser = async (req,res) => {
     }
 }
 
-export { registerUser }
+
+// API to login user
+const loginUser = async (req,res) => {
+    try {
+
+        const { email, password } = req.body
+        const user = await userModel.findOne({email})
+
+        if (!user) {
+            return res.json({success:false, message:"User does not exist"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+            res.json({success:true, token})
+        } else {
+            res.json({success:false, message:"Invalid credentials"})
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+
+// api to get user profile data
+const getProfile = async (req,res) => {
+    try {
+        
+        const { userId } = req.body
+        const userData = await userModel.findById(userId).select('-password')
+        res.json({success:true, userData})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+// APi to update user profile
+
+// api to update user profile
+const updateProfile = async (req,res) => {
+    try {
+
+        const { userId, name, phone, address, dob, gender } = req.body
+        const imageFile = req.file
+
+        if (!name || !phone || !dob || !gender) {
+            return res.json({success:false, message:"Data Missing"})
+        }
+
+        await userModel.findByIdAndUpdate(userId, {
+            name,
+            phone,
+            address: JSON.parse(address),
+            dob,
+            gender
+        })
+
+        if (imageFile) {
+            // upload image to cloudinary
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:'image'})
+            const imageURL = imageUpload.secure_url
+
+            await userModel.findByIdAndUpdate(userId, {image:imageURL})
+        }
+
+        res.json({success:true, message:"Profile Updated"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+// API  to book appointment
+
+const bookAppointment = async(req,res)=>{
+    try {
+        
+  const{userId,docId,slotDate,slotTime}=req.body
+
+  const docData = await doctorModel.findById(docId).select('-password')
+
+  if(!docData.available){
+    return res.json({success:false, message:'Doctor not available'})
+  }
+
+let slots_booked=docData.slots_booked
+
+// checking for slot availability
+ if(slots_booked[slotDate]){
+    if(slots_booked[slotDate].includes(slotTime)){
+ return res.json({success:false, message:'Slot not available'})
+    }
+    else{
+        slots_booked[slotDate].push(slotTime)
+    }
+ } else{
+    slots_booked[slotDate]=[]
+    slots_booked[slotDate].push(slotTime)
+ }
+
+const userData = await userModel.findById(userId).select('-password')
+
+
+  delete docData.slots_booked
+
+const appointmentData = {
+    userId,
+    docId,
+    userData,
+    docData,
+    amount:docData.fees,
+    slotTime,
+    slotDate,
+    date: Date.now()
+}
+
+const newAppointment = new appointmentModel(appointmentData)
+await newAppointment.save()
+
+// save new slots data in doctorData
+
+await doctorModel.findByIdAndUpdate(docId,{slots_booked})
+ 
+res.json({success:true,message:'Appointment Booked'})
+
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+
+
+
+export { registerUser, loginUser, getProfile,updateProfile }
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
 import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
 
 // app config
 
@@ -24,6 +25,7 @@ app.use('/api/admin',adminRouter)
 // localhost:4000/api/admin
 // Express checks: does this URL start with /api/admin? → Yes. So it hands off the rest of the URL (/add-doctor) to adminRouter to handle internally.
 app.use('/api/doctor',doctorRouter)
+app.use('/api/user',userRouter)
 
 app.get('/', (req, res) => {
     return res.status(200).json({
